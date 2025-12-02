7835682986daae6e4e880533516972268e62c1d4
JOAO CARLOS COSTA SOUZA <joao.ccsouza1@senacsp.edu.br>
2025-12-02 19:46:47 -0300

    Pesquisa Sequencial

---
+++ PesquisaSequencial/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PesquisaSequencial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Meu primeiro git");
+        }
+    }
+}
